62df6b1521ce7f06494b16bd90c71f38d41e6b32
Leonardo Barreiros <leossb36@gmail.com>
2021-03-21 23:27:38 -0300

    Inserindo menu de seleção
    Signed-off-by: Leonardo Barreiros <leossb36@gmail.com>
    Co-authored-by: Ian Rocha <ianrocha.df@gmail.com>

---
+++ src/python_greedy/view/menu.py
@@ -0,0 +1,9 @@
+def menu_choice():
+    print(45 * "*" , "MENU" , 45 * "*")
+    print("1. Comparacao Shortest processing time first, Earliest deadline first, smallest slack em Python")
+    print("2. Comparacao Shortest processing time first, Earliest deadline first, smallest slack em C")
+    print("3. Compara tempos dos algoritmos em C e python")
+    print("4. Gera csv com relatório dos tempos de ordenação em python")
+    print("5. Gera csv com relatório dos tempos de ordenação em C")
+    print("6. Sair")
+    print(97 * "*")
