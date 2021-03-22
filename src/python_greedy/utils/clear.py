51f28c90c6d14b18a74a86c315add82b85d86e3b
Leonardo Barreiros <leossb36@gmail.com>
2021-03-21 23:29:11 -0300

    Inserindo função para limpar terminal
    Signed-off-by: Leonardo Barreiros <leossb36@gmail.com>
    Co-authored-by: Ian Rocha <ianrocha.df@gmail.com>

---
+++ src/python_greedy/utils/clear.py
@@ -0,0 +1,9 @@
+import os
+
+def clear():
+    '''
+    Clears the terminal screen and scroll back to present
+    the user with a nice clean, new screen. Useful for managing
+    menu screens in terminal applications.
+    '''
+    os.system('cls||echo -e \\\\033c')
