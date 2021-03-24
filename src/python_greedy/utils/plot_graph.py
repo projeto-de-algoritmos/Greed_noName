53a076e9660ec4ccd392eeeeb94945a6c4792420
Leonardo Barreiros <leossb36@gmail.com>
2021-03-23 22:50:25 -0300

    Alterando funcao de plotar grafico
    Signed-off-by: Leonardo Barreiros <leossb36@gmail.com>
    Co-authored-by: Ian Rocha <ianrocha.df@gmail.com>

--- src/python_greedy/utils/plot_graph.py
+++ src/python_greedy/utils/plot_graph.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_graph(minimizing_lateness, type_language):
-    pd.DataFrame(minimizing_lateness).plot(kind='line')
-    graph_title = 'Grafico Tempo(s) x Processo em ' + type_language
-    plt.title(graph_title)
-    plt.ylabel('Tempo(s)')
-    plt.xlabel('Processo')
-    plt.show()
+def plot_graph(csvFile):
+    try:
+        data = pd.read_csv(csvFile)
+
+        plt.figure(figsize=(6.8, 4.2))
+        x = range(len(data['shortest_processing_first']))
+        plt.plot(data['shortest_processing_first'])
+        plt.plot(data['earliest_deadline_first'])
+        plt.plot(data['smallest_slack'])
+        plt.xlabel('Quantidade')
+        plt.ylabel('Tempo(ns)')
+        plt.show()
+    except Exception:
+        print('Necessario gerar csv primeiro')
